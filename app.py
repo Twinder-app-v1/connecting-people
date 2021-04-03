3a130b9c0c54b8b90fdf8dc6bbbf0ad9953abe2d
Briano Goestiawan <b@briano.io>
2021-04-03 19:28:15 +1100

    Add pages

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home_page():
-    return "hello, world"
+    return render_template("home_page.html", data=9)
+
+@app.route("/login")
+def login_page():
+    return render_template("login_page.html", data=9)
+
+@app.route("/profile")
+def profile_page():
+    return render_template("profile_page.html", data=9)
+
+@app.route("/chat")
+def chat_list_page():
+    return render_template("chat_list_page.html", data=9)
+
+@app.route("/chat/<string:chat_id>")
+def chat_page_page():
+    return render_template("chat_page_page.html", data=9)
